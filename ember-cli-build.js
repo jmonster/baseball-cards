cc73ad4de98da34ef890d9cfbe3401e871e67800
Johnny Domino <jvdomino@gmail.com>
2018-05-20 17:34:44 -0700

    serve assets from self

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,8 @@ module.exports = function(defaults) {
 
   });
 
+  app.import('vendor/purple3.min.css');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
