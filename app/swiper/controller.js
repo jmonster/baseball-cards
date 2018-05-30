f0e6bd4c25d36f6997d4c0cd9dbcbcc90637d196
Johnny Domino <jvdomino@gmail.com>
2018-05-29 17:43:35 -0700

    sick tinder cards!

--- app/swiper/controller.js
+++ app/swiper/controller.js
@@ -3,7 +3,12 @@ import { computed } from '@ember/object';
 
 export default Controller.extend({
   allTherapists: computed(function() {
-    return ['Zoidberg', 'Nick', 'Herbert']
+    return [
+      'https://vignette.wikia.nocookie.net/simpsons/images/b/bc/Dr._Monroe%27s_Family_Therapy_Center_%28There%27s_No_Disgrace_Like_Home%29.png/revision/latest?cb=20130512023251', //monroe
+      'https://vignette.wikia.nocookie.net/en.futurama/images/4/45/Dr._John_A._Zoidberg.png/revision/latest?cb=20101014052403', // zoidberg
+      'https://pbs.twimg.com/profile_images/618151052664303616/kemCYDyX_400x400.png', // nick
+      'http://i0.kym-cdn.com/entries/icons/original/000/021/264/yS2EwcX.jpg' // hibbert
+    ]
   }),
 
   paginatedTherapists: computed('allTherapists.[]', function() {
@@ -16,7 +21,12 @@ export default Controller.extend({
     },
 
     reset() {
-      this.set('allTherapists', ['Zoidberg', 'Nick', 'Herbert']);
+      this.set('allTherapists', [
+        'https://vignette.wikia.nocookie.net/simpsons/images/b/bc/Dr._Monroe%27s_Family_Therapy_Center_%28There%27s_No_Disgrace_Like_Home%29.png/revision/latest?cb=20130512023251', //monroe
+        'https://vignette.wikia.nocookie.net/en.futurama/images/4/45/Dr._John_A._Zoidberg.png/revision/latest?cb=20101014052403', // zoidberg
+        'https://pbs.twimg.com/profile_images/618151052664303616/kemCYDyX_400x400.png', // nick
+        'http://i0.kym-cdn.com/entries/icons/original/000/021/264/yS2EwcX.jpg' // hibbert
+      ]);
     }
   }
 });
