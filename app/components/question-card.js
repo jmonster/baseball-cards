8c229b1611bccdd48b639abd4a4ba0b133b3fb95
Johnny Domino <jvdomino@gmail.com>
2018-06-24 19:18:17 -0700

    more ui

--- app/components/question-card.js
+++ app/components/question-card.js
@@ -3,7 +3,7 @@ import TransitionMixin from 'ember-css-transitions/mixins/transition-mixin';
 import { computed } from '@ember/object';
 
 export default Component.extend(TransitionMixin, {
-  classNames: ['question-card flex flex-column absolute top-0 bottom-0 right-0 left-0 pa2'],
+  classNames: ['question-card ph3 absolute top-0 left-0 right-0'],
 
   transitionClass: computed(function() {
     return this.get('enableTransition') && 'wizard';
--- app/components/x-header/component.js
+++ app/components/x-header/component.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['navigation flex flex-none items-stretch gradient-purple3 w-100 fixed z-1 shadow-outer-1 white top-0 fw5']
+  classNames: ['navigation flex flex-none items-stretch gradient-purple3 w-100 fixed z-1 shadow-outer-1 white top-0 fw4']
 });
