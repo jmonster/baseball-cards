0f4aeeb152620cb2d962972883d52211e8458e54
Johnny Domino <jvdomino@gmail.com>
2018-12-14 02:34:33 -0800

    ensure two decimals when deserializing dollars

--- app/transforms/dollars.js
+++ app/transforms/dollars.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Transform.extend({
   deserialize(serialized) {
-    return serialized / 100; // returns dollars
+    return (serialized / 100).toFixed(2); // returns dollars
   },
 
   serialize(deserialized) {
