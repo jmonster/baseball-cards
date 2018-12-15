726e38b1a36ba88ba74eee817f439c82d65bbbc0
RasPhilCo <RasPhilCo@users.noreply.github.com>
2018-12-15 10:03:02 -0800

    refactor deal-list-*

--- app/models/deal.js
+++ app/models/deal.js
@@ -9,11 +9,14 @@ export default DS.Model.extend({
   tags: DS.hasMany('tag', { inverse, async: true }),
   // images: DS.hasMany('image', { inverse, async: true }),
 
-  // title: DS.attr('string'),
-  // description: DS.attr('string'),
+  title: DS.attr('string'),
+  description: DS.attr('string'),
+
+  // TODO we should offer an array of opportunities here
+  // e.g. Costco members can get a lower price than Amazon Prime can get a lower price than someone using a 20% off coupon ...
   price: DS.attr('dollars'),
-  // wiki: DS.attr('string'),
-  // url: DS.attr('string'),
+  // wiki: DS.attr('string'), // someday soon
+  url: DS.attr('string'),
 
   primaryImage: computed(function() {
     // return this.product.images.firstObject;
