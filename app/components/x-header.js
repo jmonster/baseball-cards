29b93594ad3511330adf25a1adb76a3eafd4a119
Johnny Domino <jvdomino@gmail.com>
2019-01-05 18:03:33 -0500

    show liked deal count in logo corner badge thang

--- app/components/x-header.js
+++ app/components/x-header.js
@@ -1,10 +1,13 @@
 import Component from '@ember/component';
 import { inject } from '@ember/service';
 import { computed } from '@ember/object';
+import { alias } from '@ember/object/computed';
 
 export default Component.extend({
   classNames: ['navigation', 'z-1', 'flex', 'flex-none', 'bg-gradient-primary', 'shadow-outer-1'],
   router: inject(),
+  dataService: inject(),
+
   linkToRouteName: computed('router.currentRouteName', function() {
     const currentRoute = this.get('router.currentRouteName');
 
@@ -13,5 +16,8 @@ export default Component.extend({
     } else {
       return 'app.swiper';
     }
-  })
+  }),
+
+  // TODO subtract expired deals from the count
+  likedDealCount: alias('dataService.likedDealsIds.length')
 });
