a720a3f729b5c4de1880eb7d66e14bca1d27ce3c
Johnny Domino <jvdomino@gmail.com>
2019-01-05 17:45:58 -0500

    logo toggles between deal list and swiper

--- app/components/x-header.js
+++ app/components/x-header.js
@@ -1,5 +1,17 @@
 import Component from '@ember/component';
+import { inject } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default Component.extend({
-  classNames: ['navigation', 'z-1', 'flex', 'flex-none', 'bg-gradient-primary', 'shadow-outer-1']
+  classNames: ['navigation', 'z-1', 'flex', 'flex-none', 'bg-gradient-primary', 'shadow-outer-1'],
+  router: inject(),
+  linkToRouteName: computed('router.currentRouteName', function() {
+    const currentRoute = this.get('router.currentRouteName');
+
+    if (/swiper/.test(currentRoute)) {
+      return 'app.deals';
+    } else {
+      return 'app.swiper';
+    }
+  })
 });
