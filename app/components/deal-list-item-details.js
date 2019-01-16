bd3d06c2af180d14048e58f9254965a9b63f8ce1
Johnny Domino <jvdomino@gmail.com>
2019-01-16 14:07:04 -0800

    flesh out deal-list-item view

--- app/components/deal-list-item-details.js
+++ app/components/deal-list-item-details.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['flex-auto', 'flex', 'flex-column', 'justify-between', 'pa1']
+  classNames: ['pa1']
 });
