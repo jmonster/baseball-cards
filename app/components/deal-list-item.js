ed45de1055b9aa2c078a846fae3cfa8e546cb607
Johnny Domino <jvdomino@gmail.com>
2019-02-04 15:12:30 -0800

    link deal-item to product page (instead of amazon)

--- app/components/deal-list-item.js
+++ app/components/deal-list-item.js
@@ -2,10 +2,17 @@ import Component from '@ember/component';
 import { alias } from '@ember/object/computed';
 
 export default Component.extend({
-  tagName: 'a',
-  attributeBindings: ['href', 'target'],
-  href: alias('deal.url'),
-  target: '_blank',
+  // turns this component into a link to Amazon™
+  // but currently passing in a click handler
+  // to transitionToRoute(product)
+  //
+  // tagName: 'a',
+  // attributeBindings: ['href', 'target'],
+  // href: alias('deal.url'),
+  // target: '_blank',
+  // click() {
+  //   this.transitionToRoute('product', this.get('deal.product.id'));
+  // },
 
   isExpired: alias('deal.isExpired'),
 
--- app/controllers/app/deals.js
+++ app/controllers/app/deals.js
@@ -1,4 +1,9 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  actions: {
+    onDealListItemClick(deal) {
+      this.transitionToRoute('app.product', deal.get('product.id'));
+    }
+  }
 });
