b6cabe984037ecbf9aaaa0417a1a1ddf4b7e173e
Johnny Domino <jvdomino@gmail.com>
2019-03-03 14:24:44 -0800

    remove usage of form-data

--- scripts/lib/discourse.js
+++ scripts/lib/discourse.js
@@ -1,25 +1,14 @@
 const got = require('got');
-const FormData = require('form-data');
 
-const headers = { 'Accept': 'application/json' };
 const discourse = got.extend({
   'baseUrl': 'https://messeji.dealzil.la',
   responseType: 'json'
 });
 
-
 exports.postTopic = async function({ title, raw, api_key, api_username }) {
-  const body = new FormData();
-
-  body.append('api_key', api_key);
-  body.append('api_username', api_username);
-  body.append('title', title);
-  body.append('raw', raw);
-
   return discourse.post('/posts', {
-    body,
-    searchParams: new URLSearchParams([['api_key', api_key]]),
-    responseType: 'json'
+    body: new URLSearchParams({ api_key, api_username, title, raw }).toString(),
+    searchParams: new URLSearchParams([['api_key', api_key]])
   });
 }
 
