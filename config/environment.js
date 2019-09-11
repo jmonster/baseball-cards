6e253f35be22db3db0b1bfb01959e76a1a7f99d4
Johnny Domino <jvdomino@gmail.com>
2019-09-11 19:49:48 -0400

    use mirage in production; passthrough google profile

--- config/environment.js
+++ config/environment.js
@@ -66,7 +66,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV['ember-cli-mirage'] = { enabled: false };
+    ENV['ember-cli-mirage'] = { enabled: true };
   }
 
   return ENV;
